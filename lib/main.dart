4e63d46995fa388682b6de15c2ea28078820a347
almyk <hansentommy92@gmail.com>
2019-07-26 15:21:35 +0900

    Add drawer and code to be able to view online lists for all servers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+const destiny = 0;
+const legacy = 1;
+const pendulum = 2;
+const prophecy = 3;
+const strife = 4;
+
+const serverNames = ['Destiny', 'Legacy', 'Pendulum', 'Prophecy', 'Strife'];
+
+int server = pendulum;
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -32,10 +42,17 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _buildOnline(context),
+      body: _buildBody(context),
+      drawer: _createDrawer(),
     );
   }
-  
+
+  Widget _buildBody(BuildContext context) {
+    return Container(
+        child: _buildOnline(context)
+    );
+  }
+
   Widget _buildOnline(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('online').snapshots(),
@@ -47,14 +64,35 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   
   Widget _buildOnlineList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    // TODO: update drawer with online counts
     return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildOnlineListItem(context, data)).toList(),
+      padding: const EdgeInsets.only(top: 5.0),
+      children: _buildOnlineListItems(context, snapshot[server]),
     );
   }
-  
-  Widget _buildOnlineListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
+
+  List<Widget> _buildOnlineListItems(BuildContext context, DocumentSnapshot data) {
+    List<dynamic> players = data.data['players'];
+    List<Widget> onlineList = new List<Widget>();
+
+    onlineList.add(
+        Text(
+          serverNames[server],
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
+        ));
+
+    for(final player in players) {
+      onlineList.add(_buildOnlineListItem(context, Map<String, dynamic>.from(player)));
+    }
+    return onlineList;
+  }
+
+  Widget _buildOnlineListItem(BuildContext context, Map<String, dynamic> data) {
+    final record = Record.fromMap(data);
     
     return Padding(
       key: ValueKey(record.name),
@@ -66,9 +104,72 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         child: ListTile(
           title: Text(record.name),
-          trailing: Text(record.level.toString()),
+          trailing: Text("lv: " + record.level.toString()),
+          subtitle: Text("Last login: " + record.login + ", " + record.vocation),
           onTap: () => print(record),
         ),
+      ),
+    );
+  }
+
+  Widget _createDrawer() {
+    return Drawer(
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('Medivia Things'),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: Text('Destiny'),
+            onTap: () {
+              setState(() {
+                server = destiny;
+              });
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Legacy'),
+            onTap: () {
+              setState(() {
+                server = legacy;
+              });
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Pendulum'),
+            onTap: () {
+              setState(() {
+                server = pendulum;
+              });
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Prophecy'),
+            onTap: () {
+              setState(() {
+                server = prophecy;
+              });
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Strife'),
+            onTap: () {
+              setState(() {
+                server = strife;
+              });
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
@@ -77,13 +178,19 @@ class _MyHomePageState extends State<MyHomePage> {
 class Record {
   final String name;
   final int level;
+  final String vocation;
+  final String login;
   final DocumentReference reference;
 
   Record.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['name'] != null),
         assert(map['level'] != null),
+        assert(map['vocation'] != null),
+        assert(map['login'] != null),
         name = map['name'],
-        level = map['level'];
+        level = map['level'],
+        vocation = map['vocation'],
+        login = map['login'];
 
   Record.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data, reference: snapshot.reference);
