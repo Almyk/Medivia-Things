21adf19418731421837ae0005574d1bd3ddd3ee3
almyk <hansentommy92@gmail.com>
2019-09-04 23:03:47 +0900

    Create skeleton files for bedmage page

---
+++ lib/bloc/blocs/bedmage_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:medivia_things/bloc/event/bedmage_event.dart';
+import 'package:medivia_things/bloc/state/bedmage_state.dart';
+
+class BedmageBloc extends Bloc<BedmageEvent, BedmageState> {
+  @override
+  get initialState => null; // TODO
+
+  @override
+  Stream<BedmageState> mapEventToState(BedmageEvent event) {
+    // TODO: implement mapEventToState
+    return null;
+  }
+}
---
+++ lib/pages/bedmage_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class BedmagePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
